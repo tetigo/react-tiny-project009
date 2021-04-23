5414304a4733b9c3aa03c8c0c5df897dc907fe9c
Tiago Mendes <tetigo@gmail.com>
2021-04-23 01:55:18 -0300

    making dynamic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
 function App() {
-  const size = 10
+  let [percent, setPercent] = useState(10)
   const [color, setColor] = useState('#F15025')
   const [error, setError] = useState(false)
-  const [list, setList] = useState(new Values(color).all(size))
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    const listColors = new Values(color).all(percent)
+    setList(listColors)
+  }, [percent])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hihihi')
     try {
-      const listColors = new Values(color).all(size)
-      console.log(listColors)
-      setError(false)
+      const listColors = new Values(color).all(percent)
       setList(listColors)
+      setError(false)
     } catch (error) {
       setError(true)
-      console.log(error)
     }
   }
 
@@ -29,11 +31,20 @@ function App() {
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
           <input
+            type="number"
+            style={{ marginRight: '20px', width: '100px' }}
+            value={percent}
+            onChange={(e) => {
+              let valor = parseInt(e.target.value)
+              if (valor <= 1) setPercent(1)
+              else if (valor >= 101) setPercent(101)
+              else setPercent(valor)
+            }}
+          />
+          <input
             type="text"
             value={color}
-            onChange={(e) => {
-              setColor(e.target.value)
-            }}
+            onChange={(e) => setColor(e.target.value)}
             placeholder="#F15025"
             // className={`${error ? 'error' : null}`}
             className={error ? 'error' : null}
@@ -44,10 +55,12 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        {list.map((color, idx) => {
-          console.log(color)
-          return <SingleColor key={idx} {...color} index={idx} size={size} />
-        })}
+        {list &&
+          list.map((color, idx) => {
+            return (
+              <SingleColor key={idx} {...color} index={idx} size={percent} />
+            )
+          })}
       </section>
     </>
   )
